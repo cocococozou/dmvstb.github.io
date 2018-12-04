b767ae077904d659ae0eee2745ae1bd8cbb7055c
Xiaoke Zou <xiaokezou@Xiaokes-MacBook-Pro.local>
2018-12-03 23:28:13 -0500

    successful public!!

--- km/app.R
+++ km/app.R
@@ -7,24 +7,28 @@
 #    http://shiny.rstudio.com/
 #
 
+library(flexdashboard)
 library(shiny)
 library(p8105.datasets)
 library(plotly)
 library(tidyverse)
 library(viridis)
+library(survminer)
+library(survival)
 library(flexdashboard)
+library(dplyr)
 
-data("nyc_airbnb")
 
-nyc_airbnb = 
-  nyc_airbnb %>% 
-  mutate(rating = review_scores_location / 2) %>%
-  select(boro = neighbourhood_group, neighbourhood, rating, price, room_type,
-         latitude, longitude) %>%
-  filter(!is.na(rating))
-boros = nyc_airbnb %>% distinct(boro) %>% pull()
+load('./df_combine.RData')
 
-risk_vectors <- c("drug_level","comlications","exercise_level")
+
+df_combine$survival = with(df_combine, Surv(days, tb == "Yes"))
+
+df_combine <- df_combine %>% 
+  head(10)
+
+risk_vectors <- c("drug","gender")
+district_vectors <- c("A","B","C")
 
 
 # Define UI for application that draws a histogram
@@ -32,24 +36,40 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("K-M analysis"),
-   
-   selectInput("risk_factor", label = h3("Risk Factors"),
-               choices = risk_vectors, selected = "Manhattan")
-   
+   sidebarPanel(
+     selectInput("risk_factor", label = h3("Risk Factors"),
+                 choices = risk_vectors, selected = "Manhattan"),
+     
+     selectInput("district_vector", label = h3("Select District"),
+                 choices = district_vectors, selected = "A")
+   ),
+   mainPanel(
+     plotOutput('plot1')
+   )
    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$plot1 <- renderPlot(
+    if(input$risk_factor=="gender"){
+      km =  survfit(survival ~ gender, data = df_combine, conf.type = "log-log")
+      ggsurvplot(km, 
+                 data = df_combine, 
+                 risk.table = F, 
+                 pval = T, 
+                 ylab = paste0("o","1"), 
+                 ylim = c(0.9, 1.0))
+    }else{
+      km =  survfit(survival ~ drug, data = df_combine, conf.type = "log-log")
+      ggsurvplot(km, 
+                 data = df_combine, 
+                 risk.table = F, 
+                 pval = T, 
+                 ylab = paste0("o","1"), 
+                 ylim = c(0.9, 1.0))
+    }
+  )
 }
 
 # Run the application 
